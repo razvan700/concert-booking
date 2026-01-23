50d1e73a2fa8f270d340567594ef285fd54b44cb
Horia Stanescu <horia.stanescualex@gmail.com>
2026-01-23 07:37:42 +0200

    Created entities for artists and concerts

---
+++ src/main/java/com/example/concertbookingapplication/entity/Concert.java
@@ -0,0 +1,27 @@
+package com.example.concertbookingapplication.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+public class Concert {
+
+    @Id
+    private UUID id;
+
+    private String name;
+
+    @OneToMany
+    private List<Artist> artists;
+
+}
