85b13495636627b6fdc568a098f33cefaf00ce6c
razvan700 <razvan_c_popescu57@yahoo.com>
2026-01-29 22:37:53 +0200

    Updated artist not found exception impl

--- src/main/java/com/example/concertbookingapplication/exception/ArtistNotFoundException.java
+++ src/main/java/com/example/concertbookingapplication/exception/ArtistNotFoundException.java
@@ -2,9 +2,12 @@ package com.example.concertbookingapplication.exception;
 
 import java.util.UUID;
 
-public class ArtistNotFoundException extends  ApplicationException {
-    public ArtistNotFoundException(UUID id) {
+public class ArtistNotFoundException extends ApplicationException {
 
-        super("Artist with id " + id + " not found!");
+    public ArtistNotFoundException(UUID id) {
+        super(
+                "Artist with id " + id + " not found!",
+                404
+        );
     }
 }
